ec9971201c074564e846571da4d2dc3f5092f79d
Alexxx2207 <alex.ivanov0722@gmail.com>
2025-12-07 02:33:54 +0200

    Change chart update to not jump

--- client/src/components/chart/index.tsx
+++ client/src/components/chart/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { instrumentService, type Tick } from "../../services/instruments-service";
-import { CandlestickSeries, ColorType, createChart } from "lightweight-charts";
+import { CandlestickSeries, ColorType, createChart, type ISeriesApi } from "lightweight-charts";
 import { ticksToMinuteCandles } from "../../utils/converters";
 import { useAsync } from "../../hooks/useAsync";
 import { io } from "socket.io-client";
@@ -14,14 +14,17 @@ const socket = io("http://localhost:5000", { transports: ["websocket"] });
 
 export function Chart({ instrument, height=500 } : ChartProps) {
   const chartContainerRef = useRef<HTMLDivElement | null>(null);
+  const seriesRef = useRef<ISeriesApi<"Candlestick"> | null>(null);
+
+
   const [ticksCount, setTicksCount] = useState(100)
   const [ticks, setTicks] = useState<Tick[]>([])
 
   const { reload } = useAsync(async () => setTicks(await instrumentService.getData(instrument, ticksCount)), [])
 
   useEffect(() => {
-    const container = chartContainerRef.current;
-    if (!container || !ticks) return;
+    const container = chartContainerRef.current
+    if (!container) return
 
     const chart = createChart(container, {
       layout: {
@@ -30,7 +33,7 @@ export function Chart({ instrument, height=500 } : ChartProps) {
       },
       width: container.clientWidth,
       height: height,
-    });
+    })
 
     const candlestickSeries = chart.addSeries(CandlestickSeries, {
       upColor: '#309e0eff',
@@ -38,47 +41,56 @@ export function Chart({ instrument, height=500 } : ChartProps) {
       borderVisible: false,
       wickUpColor: '#309e0eff',
       wickDownColor: '#ef5350',
-    });
+    })
 
-    const candles = ticksToMinuteCandles(ticks);
+    const candles = ticksToMinuteCandles(ticks)
 
-    candlestickSeries.setData(candles);
+    candlestickSeries.setData(candles)
+    seriesRef.current = candlestickSeries
+
+    chart.timeScale().fitContent()
     
-    chart.timeScale().fitContent();
-
     const handleResize = () => {
       if (!chartContainerRef.current) return;
       chart.applyOptions({ width: chartContainerRef.current.clientWidth });
-    };
+    }
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener('resize', handleResize)
 
     return () => {
-      window.removeEventListener('resize', handleResize);
-      chart.remove();
+      window.removeEventListener('resize', handleResize)
+      chart.remove()
     };
-  }, [ticks]);
+  }, [])
+
+  useEffect(() => {
+  if (!seriesRef.current) return
+
+  const candles = ticksToMinuteCandles(ticks)
+  seriesRef.current.setData(candles)
+
+}, [ticks]);
 
   useEffect(() => {
     socket.on("connect", () => {
-      console.log("connected:", socket.id);
-      socket.emit("subscribe", { symbol: "ES" });
-    });
+      console.log("connected:", socket.id)
+      socket.emit("subscribe", { symbol: "ES" })
+    })
 
     socket.on("disconnect", (reason) => {
-      console.log("disconnected:", reason);
-    });
+      console.log("disconnected:", reason)
+    })
 
     socket.on("price", (tick) => {
       setTicks(old => [...old, tick])
-    });
+    })
 
     return () => {
       socket.off("connect");
       socket.off("disconnect");
       socket.off("price");
-    };
-  }, []);
+    }
+  }, [])
 
   return (
     <div>
